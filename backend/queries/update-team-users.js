f3abc08446284485350a41ef78c3b28097a3b6bf
Carl-Emil <rohdee@icloud.com>
2024-06-04 15:46:35 +0200

    Refactor update-team-users query to use dynamic SQL inputs

--- backend/queries/update-team-users.js
+++ backend/queries/update-team-users.js
@@ -7,29 +7,50 @@ async function UpdateTeamUsers(teamUserData) {
     let pool = await sql.connect(config);
 
     // Prepare the SQL query with placeholders for each field
-    const query = `
+    let query = `
       UPDATE [dev_pingodocs_dk_db_prod].[dbo].[Customer]
       SET 
-        UserSubscriptionType = @UserSubscriptionType,
-        IsFreemiumPlusMember = @IsFreemiumPlusMember,
-        IsFreemiumPlusComplyMember = @IsFreemiumPlusComplyMember,
-        IsOfficeCommunityUser = @IsOfficeCommunityUser,
-        OfficeCommunityName = @OfficeCommunityName
+    `;
+
+    let inputs = pool.request();
+
+    if (teamUserData.UserSubscriptionType !== "") {
+      query += `UserSubscriptionType = @UserSubscriptionType,`;
+      inputs = inputs.input('UserSubscriptionType', sql.Int, teamUserData.UserSubscriptionType);
+    }
+
+    if (teamUserData.IsFreemiumPlusMember !== "") {
+      query += `IsFreemiumPlusMember = @IsFreemiumPlusMember,`;
+      inputs = inputs.input('IsFreemiumPlusMember', sql.Int, teamUserData.IsFreemiumPlusMember);
+    }
+
+    if (teamUserData.IsFreemiumPlusComplyMember !== "") {
+      query += `IsFreemiumPlusComplyMember = @IsFreemiumPlusComplyMember,`;
+      inputs = inputs.input('IsFreemiumPlusComplyMember', sql.Int, teamUserData.IsFreemiumPlusComplyMember);
+    }
+
+    if (teamUserData.IsOfficeCommunityUser !== "") {
+      query += `IsOfficeCommunityUser = @IsOfficeCommunityUser,`;
+      inputs = inputs.input('IsOfficeCommunityUser', sql.Int, teamUserData.IsOfficeCommunityUser);
+    }
+
+    if (teamUserData.OfficeCommunityName !== "") {
+      query += `OfficeCommunityName = @OfficeCommunityName,`;
+      inputs = inputs.input('OfficeCommunityName', sql.NVarChar, teamUserData.OfficeCommunityName);
+    }
+
+    // Remove the last comma and add the rest of the query
+    query = query.slice(0, -1) + `
       FROM [dev_pingodocs_dk_db_prod].[dbo].[Customer]
       INNER JOIN [dev_pingodocs_dk_db_prod].[dbo].[1TeamUser]
       ON [dev_pingodocs_dk_db_prod].[dbo].[Customer].Id = [dev_pingodocs_dk_db_prod].[dbo].[1TeamUser].CustomerId
       WHERE [dev_pingodocs_dk_db_prod].[dbo].[1TeamUser].TeamId = @SpecificTeamId;
     `;
 
+    inputs = inputs.input('SpecificTeamId', sql.Int, teamUserData.SpecificTeamId);
+
     // Execute the query with inputs
-    const result = await pool.request()
-      .input('UserSubscriptionType', sql.Int, teamUserData.UserSubscriptionType)
-      .input('IsFreemiumPlusMember', sql.Int, teamUserData.IsFreemiumPlusMember)
-      .input('IsFreemiumPlusComplyMember', sql.Int, teamUserData.IsFreemiumPlusComplyMember)
-      .input('IsOfficeCommunityUser', sql.Int, teamUserData.IsOfficeCommunityUser)
-      .input('OfficeCommunityName', sql.NVarChar, teamUserData.OfficeCommunityName)
-      .input('SpecificTeamId', sql.Int, teamUserData.SpecificTeamId)
-      .query(query);
+    const result = await inputs.query(query);
     
     // Return a success message with the number of users updated
     return { message: `${result.rowsAffected[0]} users updated successfully!`};
