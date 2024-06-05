3889178ba60c5ab9aae2bbd303da0477c516d5ce
Carl-Emil <rohdee@icloud.com>
2024-06-05 21:11:37 +0200

    Add logo to Sidebar component

--- frontend/src/components/Sidebar.svelte
+++ frontend/src/components/Sidebar.svelte
@@ -5,10 +5,10 @@
     import UpdateUserForm from './UpdateTeamUsers.svelte';
     import DeleteTeams from './DeleteTeams.svelte';
     import LineChart from './LineChart.svelte';
-    import Frontpage from './Frontpage.svelte'; // import the Frontpage component
+    import Frontpage from './Frontpage.svelte';
 
     const components = writable([
-        { name: 'Frontpage', component: Frontpage }, // add the Frontpage tab at the top
+        { name: 'Frontpage', component: Frontpage }, 
         { name: 'Onboarding', component: UserForm },
         { name: 'Update customers', component: UpdateUserForm },
         { name: 'Delete Teams', component: DeleteTeams },
@@ -20,12 +20,14 @@
         selectedComponent = value[0];
     });
 
-    let imageUrl = 'https://app.pingodocs.dk/CustomerSpecific/logo.png'; // replace with your image URL
+    let imageUrl = 'https://app.pingodocs.dk/CustomerSpecific/logo.png';
 </script>
 
 <div class="w-64 h-screen fixed left-0 top-0 bg-gray-100">
-    <div class="flex justify-center mt-4 mb-4"> <!-- Add margin-top (mt-4) here -->
-        <img src={imageUrl} alt="Logo" class="w-5/6 h-auto">
+    <div class="flex justify-center mt-4 mb-4">
+        <a href="https://www.pingodocs.dk/" target="_blank" class="flex justify-center">
+            <img src={imageUrl} alt="Logo" class="w-5/6 h-auto">
+        </a>
     </div>
     {#each $components as item (item.name)}
         <div class="p-2 cursor-pointer hover:bg-gray-200 {selectedComponent === item ? 'active' : ''}" on:click={() => (selectedComponent = item)}>
@@ -38,6 +40,6 @@
 
 <style>
 .active {
-    background-color: #ddd; /* Change this to your preferred color */
+    background-color: #ddd;
 }
 </style>
